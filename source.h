b5a2c9f265ddbed0d3d74386e4f411a2fe8dc27b
AnnRodas <100738953+AnnRodas@users.noreply.github.com>
2026-05-22 17:50:31 -0600

    kmonitor.h

--- source.h
+++ source.h
@@ -1,1 +1,23 @@
+#ifndef KMONITOR_H
+#define KMONITOR_H
 
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/mm.h>
+#include <linux/sched/signal.h>
+#include <linux/sched/task.h>
+
+#define NOMBRE_PROC "kmonitor_proyecto"
+
+/* Declaraciones de funciones */
+static const char *estado_proceso(long estado);
+static int mostrar_kmonitor(struct seq_file *m, void *v);
+static int abrir_kmonitor(struct inode *inode, struct file *file);
+static int __init iniciar_kmonitor(void);
+static void __exit salir_kmonitor(void);
+
+#endif 
+//kmonitor.h
